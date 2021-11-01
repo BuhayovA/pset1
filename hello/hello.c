859812069641985bc851365014666afafd94a796
neznayka <neznaykagm@gmail.com>
2021-11-01 08:38:08 +0200

    cash creit hello solutions

---
+++ hello/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(void)
+{
+    //get_string allows us to ask the user to enter some text
+    string name = get_string("What is your name?\n");
+
+    //printf just outputs a message to the terminal
+    printf("hello, %s\n", name);
+}
